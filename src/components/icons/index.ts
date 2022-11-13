6f4043e75077f2955f3731077d2c81d106dfb875
彭水平 <38526725@qq.com>
2022-11-13 12:06:10 +0800

    增加Icon图标组件

---
+++ src/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as IconSearch } from "./search";
+export { default as IconClose } from "./close";
+
---
+++ src/components/icons/search.tsx
@@ -0,0 +1,11 @@
+
+
+const IconSearch = ({ ...props }) => {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props} stroke-width="3" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <circle cx="10" cy="10" r="7"></circle>
+    <line x1="21" y1="21" x2="15" y2="15"></line>
+  </svg>)
+}
+
+export default IconSearch;
